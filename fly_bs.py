6e035675ad4a150c351c927f31c686b982bc9634
gtwell <gtwell@163.com>
2020-11-01 22:36:00 +0800

    Add files via upload

--- fly_bs.py
+++ fly_bs.py
@@ -1,25 +1,25 @@
 '''
 Author: gtwell
 Date: 2020-08-13 17:09:01
-LastEditTime: 2020-08-13 19:31:42
+LastEditTime: 2020-10-31 16:40:19
 '''
 import baostock as bs
 import pandas as pd
 import datetime 
 from chinese_calendar import is_workday
 import numpy as np
-from get_stocks import get_ins148_north50_data, get_hs300_zz500_data
+from get_stocks import get_better_stocks, get_hs300_zz500_data
 from tqdm import tqdm
 import os
 
 np.set_printoptions(precision=3, suppress=True)
 
-# 获得距离当日前二十一日的日期（周末，节假日剔除）
+# 获得距离当日前五十日的日期（周末，节假日剔除）
 def days_of_twentyone(today_date):
     cnt = 0
     prev = today_date
 
-    while (cnt < 24):
+    while (cnt < 50):
         if is_workday(prev):
             cnt += 1
         prev -= datetime.timedelta(days=1)
@@ -46,9 +46,9 @@ def buy_point_info(data):
     yest2_twenty_mean = np.mean(close_data[-23:-3])
 
     if ((today_five_mean / today_twenty_mean) > 1.000) and \
-        ((yest0_five_mean / yest0_twenty_mean) < 0.999) and \
-        ((yest1_five_mean / yest1_twenty_mean) < 0.998) and \
-        ((yest2_five_mean / yest2_twenty_mean) < 0.997):
+        ((yest0_five_mean / yest0_twenty_mean) < 1.000) and \
+        ((yest1_five_mean / yest1_twenty_mean) < 1.000) and \
+        ((yest2_five_mean / yest2_twenty_mean) < 1.000):
         buy_point = True
 
     if (today_five_mean < today_twenty_mean):
@@ -83,44 +83,43 @@ if __name__ == '__main__':
 
     twentyone_days_ago, now = days_of_twentyone(datetime.datetime.now())
 
-    if not os.path.exists(now):
-        os.makedirs(now)
+    if not os.path.exists(now.replace("-", "/")):
+        os.makedirs(now.replace("-", "/"))
 
     # 机构重仓 + 北上50 + 隐形冠军 + 自选
-    stocks_result = get_ins148_north50_data() # get_hs300_zz500_data(), get_ins148_north50_data()
+    stocks_result = get_better_stocks() # get_hs300_zz500_data(), get_better_stocks()
     bonds_code = stocks_result['code']
-    f = open(os.path.join(now, '精选-buy-{}.txt'.format(now)), 'w')
+    f = open(os.path.join(now.replace("-", "/"), '精选-buy-{}.txt'.format(now)), 'w')
 
     for i in tqdm(range(len(bonds_code)), ncols=80):
         data = get_hist_data(bonds_code[i], start=twentyone_days_ago, end=now)
         is_buy_point, mean_info, _ = buy_point_info(data)
-        if is_buy_point:
-            print(bonds_code[i], stocks_result["code_name"][i],
-                "今收:", data['close'].astype('float').values.tolist()[-1],
-                "今5日均线:", np.around(mean_info[0], decimals=2),
-                "今20日均线:", np.around(mean_info[1], decimals=2),
-                "昨5日均线:", np.around(mean_info[2], decimals=2),
-                "昨20日均线:", np.around(mean_info[3], decimals=2),
-                file=f)  
+        close_data = data['close'].astype('float').values.tolist()[-1] # 当日收盘价
+        if is_buy_point and close_data < 200:
+            print(bonds_code[i], stocks_result["code_name"][i], "收盘价:", close_data, file=f)          
+        # if is_buy_point:
+        #     print(np.mean(data['close'][-20:].astype('float').values))
+        #     print(bonds_code[i], stocks_result["code_name"][i],
+        #         "今收:", data['close'].astype('float').values.tolist()[-1],
+        #         "今5日均线:", np.around(mean_info[0], decimals=2),
+        #         "今20日均线:", np.around(mean_info[1], decimals=2),
+        #         "昨5日均线:", np.around(mean_info[2], decimals=2),
+        #         "昨20日均线:", np.around(mean_info[3], decimals=2),
+        #         file=f)  
     f.close()
 
     # hs300 + zz500
     stocks_result = get_hs300_zz500_data() # get_hs300_zz500_data(), get_ins148_north50_data()
     # bonds_code = ["000625", "002739"]
     bonds_code = stocks_result['code']
-    f = open(os.path.join(now, 'hs300-zz500-buy-{}.txt'.format(now)), 'w')
+    f = open(os.path.join(now.replace("-", "/"), 'hs300-zz500-buy-{}.txt'.format(now)), 'w')
 
     for i in tqdm(range(len(bonds_code)), ncols=80):
         data = get_hist_data(bonds_code[i], start=twentyone_days_ago, end=now)
         is_buy_point, mean_info, _ = buy_point_info(data)
-        if is_buy_point:
-            print(bonds_code[i], stocks_result["code_name"][i],
-                "今收:", data['close'].astype('float').values.tolist()[-1],
-                "今5日均线:", np.around(mean_info[0], decimals=2),
-                "今20日均线:", np.around(mean_info[1], decimals=2),
-                "昨5日均线:", np.around(mean_info[2], decimals=2),
-                "昨20日均线:", np.around(mean_info[3], decimals=2),
-                file=f) 
+        close_data = data['close'].astype('float').values.tolist()[-1] # 当日收盘价
+        if is_buy_point and close_data < 200:
+            print(bonds_code[i], stocks_result["code_name"][i], "收盘价:", close_data, file=f)           
     f.close()
 
     #### 登出系统 ####
